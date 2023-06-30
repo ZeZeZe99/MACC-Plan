6083ac7dab1e1d773132f62b085e1fbc5a03d896
ZeZeZe99 <rhuang@oberlin.edu>
2023-06-30 16:27:00 -0400

    Improve heuristic
    1. consider d-support (at all lower levels)
    2. goal grouping

--- A*.py
+++ A*.py
@@ -10,35 +10,35 @@ import config
 Valid degree:
     0: all valid locations reachable
     1: valid locations within shadow region
+    2: valid locations within shadow region, cannot remove goal blocks
+    3: valid locations within shadow region, cannot place a scaffold block twice
 Heuristic mode:
     0: # of plan blocks not placed + # of scaffold blocks
-    1: # of plan blocks not placed + # of scaffold blocks scaled by shadow value
-    2: # of plan blocks not placed + # of scaffold blocks + 2 * # of required scaffold blocks not placed
+    1: # of plan blocks not placed + # of scaffold blocks + 2 * sum (goal value)
 Order mode:
     0: f -> h -> generation order
-    1: f -> h -> shadow value -> generation order
-    2: f -> h -> unlock number -> generation order
 '''
-valid_mode = 1
-valid_degree = 1
-heu_mode = 0
+valid_degree = 2
+heu_mode = 1
 order_mode = 0
 
 def heuristic(height, mode=0):
     """
     Calculate heuristic value for a given height map and a goal map
     Mode 0: # of plan blocks not placed + # of scaffold blocks
-    Mode 1: # of plan blocks not placed + # of scaffold blocks scaled by shadow value
-    Mode 2: # of plan blocks not placed + # of scaffold blocks + 2 * # of required scaffold blocks not placed
+    Mode 1: # of plan blocks not placed + # of scaffold blocks + 2 * sum (goal value)
     Notes:
         Mode 1 and 2 only used for initial state
     """
     if mode == 0:
         return np.abs(height - env.goal).sum()
     elif mode == 1:
-        return env.goal.sum()
-    elif mode == 2:
-        return env.goal.sum() + 2 * len(env.required_scaf_loc)
+        # 1.0: only consider support at neighboring level (1-support)
+        # return np.abs(height - env.goal).sum() + 2 * env.get_goal_val_nb(height).sum()
+        # 1.1: consider support at all levels (d-support)
+        # return np.abs(height - env.goal).sum() + 2 * env.get_goal_val(height).sum()
+        # 1.2: consider d-support, and use goal groupings
+        return np.abs(height - env.goal).sum() + 2 * env.get_goal_val_group(height).sum()
     else:
         raise NotImplementedError
 
@@ -58,107 +58,39 @@ def heuristic_diff(loc, h, add, mode=0):
             return -1
         else:
             return 1
-    elif mode == 1:
-        if add and scaffold:
-            return 1/env.shadow_vald[(h, loc[0], loc[1])]
-        elif add and not scaffold:
-            return -1
-        elif not add and scaffold:
-            return - 1/env.shadow_vald[(h-1, loc[0], loc[1])]
-        else:
-            return 1
     else:
         raise NotImplementedError
-
-def scaf_val_diff(loc, h, add):
-    """
-    Calculate difference of scaffold value after adding or removing a block
-    scaffold value = 1 / shadow value
-    """
-    if add:
-        scaffold = h + 1 > env.goal[loc[0], loc[1]]
-    else:
-        scaffold = h > env.goal[loc[0], loc[1]]
-
-    if scaffold and add:
-        return 1/env.shadow_vald[(h, loc[0], loc[1])]
-    elif scaffold and not add:
-        return -1/env.shadow_vald[(h-1, loc[0], loc[1])]
-    else:
-        return 0
-
-def check_unlock(node):
-    """Check if the action taken by the node unlocks a new plan block"""
-    if node.unlock > 0:
-        unlock = (node.valid_goal - node.parent.valid_goal) > 0  # unlocked goal blocks
-        locs = np.transpose(np.nonzero(unlock))
-        for (x, y) in locs:
-            if (node.height[x, y], x, y) not in env.unlocked_loc:  # make sure the goal block is not unlocked before
-                env.unlocked_loc.add((node.height[x, y], x, y))
-                xs, ys = np.transpose(np.nonzero(node.height - node.parent.height))[0]
-                h = node.height[xs, ys] - 1
-                env.required_scaf_loc.add((h, xs, ys))
-                return h, xs, ys
-    return None
-
-def update_h_vals(loc, open_list):
-    """Update all h values in open list, after specifying a new required scaffold block"""
-    new_open_list = []
-    h, x, y = loc
-    for item in open_list:
-        node = item[-1]
-        if node.height[x, y] < h + 1:
-            node.h += 2
-            push_node(new_open_list, node, mode=order_mode)
-        else:
-            push_node(new_open_list, node, mode=order_mode)
-    return new_open_list
 
 def push_node(open_list, node, mode=0):
     """
     Push node to open list
     Sort order (increasing):
         Mode 0: f, h, gen_id
-        Mode 1: f, h, scaffold value, gen_id
-        Mode 2: f, h, - # unlock, gen_id
     """
     f = node.g + node.h
     if mode == 0:
         heapq.heappush(open_list, (f, node.h, node.gen_id, node))
-    elif mode == 1:
-        heapq.heappush(open_list, (f, node.h, node.scaf_val, node.gen_id, node))
-    elif mode == 2:
-        heapq.heappush(open_list, (f, node.h, -node.unlock, node.gen_id, node))
+    else:
+        raise NotImplementedError
 
 def validate(height, new_height, loc, add):
     """
     Validate a block action:
         must exist a neighbor location with correct height, and is reachable before and after the action
     """
-    new_valid = env.valid_action(new_height, mode=valid_mode, degree=valid_degree)
+    new_valid = env.valid_bfs_map(new_height, degree=valid_degree)
     x, y = loc
     h = height[x, y]
     valid = False
-    if valid_mode == 0:
-        for (x2, y2) in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
-            if (x2, y2) not in env.valid_loc:
-                continue
-            if add and height[x2, y2] == h and (x2, y2) in new_valid[0]:
-                valid = True
-                break
-            if not add and height[x2, y2] == h-1 and (x2, y2) in new_valid[0]:
-                valid = True
-                break
-    elif valid_mode == 1:
-        for (x2, y2) in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
-            if (x2, y2) not in env.valid_loc:
-                continue
-            if add and height[x2, y2] == h and new_valid[0, x2, y2]:
-                valid = True
-                break
-            if not add and height[x2, y2] == h-1 and new_valid[0, x2, y2]:
-                valid = True
-                break
+    for (x2, y2) in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
+        if (x2, y2) not in env.valid_loc:
+            continue
+        if add and height[x2, y2] == h and new_valid[0, x2, y2]:
+            valid = True
+            break
+        if not add and height[x2, y2] == h-1 and new_valid[0, x2, y2]:
+            valid = True
+            break
     return valid, new_valid
 
 def get_plan(node):
@@ -180,7 +112,7 @@ def a_star():
     open_list = []
     closed_list = dict()  # key: (height, g), value: Node
 
-    valid = env.valid_action(env.height, mode=valid_mode, degree=valid_degree)
+    valid = env.valid_bfs_map(env.height, degree=valid_degree)
     root = Node(None, env.height, valid, 0, heuristic(env.height, mode=heu_mode), 0)
     push_node(open_list, root,  mode=order_mode)
     closed_list[root.height.tobytes()] = 0
@@ -199,15 +131,21 @@ def a_star():
             print(f'Generated: {gen}, Expanded: {expand}, Invalid: {invalid}, Duplicate: {dup}, Duplicate2: {dup2}')
             return get_plan(node)
 
-        '''Dynamic heuristic: update'''
-        require_scaf = check_unlock(node)
-        if require_scaf:
-            open_list = update_h_vals(require_scaf, open_list)
-
         '''Search for child nodes'''
-        if valid_mode == 0:
-            for action in node.valid[1]:
-                new_height = env.execute(node.height.copy(), action[:2], action[2])
+        for a in range(1, 3):
+            add = a == 1
+            for (x, y) in [(x, y) for x in range(env.w) for y in range(env.w)]:
+                '''Validate action'''
+                if not node.valid[a, x, y]:
+                    continue
+                # Skip removing goal blocks
+                if valid_degree == 2 and not add and node.height[x, y] <= env.goal[x, y]:
+                    continue
+                # Skip adding a scaffold twice
+                if valid_degree == 3 and add and node.height[x, y] >= env.goal[x, y]:
+                    if (node.height[x, y], x, y) in node.added_scaffold:
+                        continue
+                new_height = env.execute(node.height.copy(), (x, y), add)
                 new_g = node.g + 1
                 new_height_bytes = new_height.tobytes()
                 # Duplicate detection: only add duplicates to open list if it has a lower g value (may add multiple)
@@ -215,44 +153,21 @@ def a_star():
                     dup += 1
                     continue
                 # Valid path detection: agent should have a way back
-                valid, new_valid = validate(node.height, new_height, action[:2], action[2])
+                valid, new_valid = validate(node.height, new_height, (x, y), add)
                 if not valid:
                     invalid += 1
                     continue
-                # Generate new node
-                new_h = heuristic(new_height, mode=heu_mode)
+                '''Generate child node'''
                 gen += 1
+                # new_h = node.h + heuristic_diff((x, y), node.height[x, y], add, mode=heu_mode)
+                new_h = heuristic(new_height, mode=heu_mode)
                 new_node = Node(node, new_height, new_valid, new_g, new_h, gen)
+                # Mark added scaffold (can only add once)
+                # if valid_degree == 3 and add and new_height[x, y] > env.goal[x, y]:
+                #     new_node.added_scaffold.add((new_height[x, y] - 1, x, y))
+
                 push_node(open_list, new_node, mode=order_mode)
                 closed_list[new_height_bytes] = new_g
-        elif valid_mode == 1:
-            for (x, y) in [(x, y) for x in range(env.w) for y in range(env.w)]:
-                for a in range(1, 3):
-                    if node.valid[a, x, y]:
-                        add = a == 1
-                    else:
-                        continue
-                    new_height = env.execute(node.height.copy(), (x, y), add)
-                    new_g = node.g + 1
-                    new_height_bytes = new_height.tobytes()
-                    # Duplicate detection: only add duplicates to open list if it has a lower g value (may add multiple)
-                    if new_height_bytes in closed_list and new_g >= closed_list[new_height_bytes]:
-                        dup += 1
-                        continue
-                    # Valid path detection: agent should have a way back
-                    valid, new_valid = validate(node.height, new_height, (x, y), add)
-                    if not valid:
-                        invalid += 1
-                        continue
-                    # Generate new node
-                    gen += 1
-                    new_h = node.h + heuristic_diff((x, y), node.height[x, y], add, mode=heu_mode)
-                    new_node = Node(node, new_height, new_valid, new_g, new_h, gen)
-                    # scaf_val = scaf_val_diff((x, y), node.height[x, y], add)
-                    # new_node.scaf_val = node.scaf_val + scaf_val
-                    # new_node.scaf_val = abs(scaf_val)
-                    push_node(open_list, new_node, mode=order_mode)
-                    closed_list[new_height_bytes] = new_g
 
     raise ValueError('No solution found')
 
@@ -264,14 +179,6 @@ class Node:
         self.g = g_val
         self.h = h_val
         self.gen_id = gen_id
-
-        self.scaf_val = 0
-        self.valid_goal = (valid[1] * (env.goal > height))
-        if parent is None:
-            self.unlock = 0
-        else:
-            self.unlock = self.valid_goal.sum() - parent.valid_goal.sum()
-        # self.unlock = max(self.unlock, 0)
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -10,61 +10,6 @@ def get_parser():
     parser.add_argument('--num', type=int, default=1, help='Number of agents')
     parser.add_argument('--cost', type=float, default=.1, help='Base cost of an action')
 
-    '''Network'''
-    parser.add_argument('--input_dim', type=int, default=2, help='Input dimension')
-
-    '''Learning'''
-    parser.add_argument('--algo', type=str, default='A2C', help='RL algorithm')
-    parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate')
-    parser.add_argument('--gamma', type=float, default=0.9, help='Discount factor')
-    parser.add_argument('--clip', type=float, default=10, help='Gradient clipping')
-    parser.add_argument('--entropy', type=float, default=0.2, help='Entropy loss coefficient')
-
-    parser.add_argument('--episode', type=int, default=10000, help='Episode number')
-    parser.add_argument('--epi_len', type=int, default=100, help='Episode length')
-    parser.add_argument('--train_step', type=int, default=100, help='Training frequency (per _ steps)')
-    parser.add_argument('--test_freq', type=int, default=1000, help='Testing frequency (per _ episodes)')
-    parser.add_argument('--test_epi', type=int, default=10, help='Testing episode number')
-    parser.add_argument('--test', action='store_true', help='Test only')
-    parser.add_argument('--save_freq', type=int, default=10000, help='Saving frequency (per _ episodes)')
-    parser.add_argument('--save_dir', type=str, default='', help='Directory for model saving')
-    parser.add_argument('--load', type=str, default='', help='Path to load a model')
-
-    parser.add_argument('--R', type=float, default=0, help='Radiation reward')
-    parser.add_argument('--H', type=float, default=0, help='Height reward')
-    parser.add_argument('--T', type=float, default=0, help='Trap reward')
-
-    parser.add_argument('--translate', action='store_true', help='Translate observation to (0, 0) corner')
-
-    '''Logging'''
-    parser.add_argument('--wandb', action='store_true', help='Log with wandb')
-    parser.add_argument('--comment', type=str, default='', help='Comment for the run')
     parser.add_argument('--gif', action='store_true', help='Save gif of the episode')
 
     return parser
-
-def process(arg):
-    arg.num_action = arg.w * arg.w * 2
-
-    arg.project = 'simple'
-    arg.group = f'w{arg.w}h{arg.h}'
-
-    tag = ''
-    key = ['t', 'R', 'H', 'T']
-    for (i, k) in enumerate([arg.translate, arg.R, arg.H, arg.T]):
-        if k == 0:
-            continue
-        if k == 1:
-            tag += key[i]
-        else:
-            tag += f'{key[i]}{k}'
-    if len(tag) > 0:
-        tag = f'-{tag}'
-    if len(arg.comment) > 0:
-        arg.comment = f'-{arg.comment}'
-    arg.name = f'{arg.algo}{tag}{arg.comment}'
-
-    if arg.save_dir == '':
-        arg.save_dir = f'save/{arg.project}/{arg.group}/'
-
-    return arg
